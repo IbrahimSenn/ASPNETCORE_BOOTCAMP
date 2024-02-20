d8a1fe42bd2005a363f7e8d3ff275bcbeacdc79e
Monsterter <ibrahimsenc@gmail.com>
2024-02-21 02:03:57 +0300

    till Edit

--- efcore/Controllers/KursController.cs
+++ efcore/Controllers/KursController.cs
@@ -1,5 +1,6 @@
 using efcore.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace efcore.Controllers //Önce controller ve sonra view oluşturulur.  
 {
@@ -11,6 +12,9 @@ namespace efcore.Controllers //Önce controller ve sonra view oluşturulur.
             _context = context;
         }
 
+         public async Task<IActionResult> Index(){ //burdan sonra view'e
+             return View(await _context.Kurslar.ToListAsync());
+         }
 
         public IActionResult Create(){ //bu aslında bir get işlemidir.
             return View();
@@ -21,9 +25,56 @@ namespace efcore.Controllers //Önce controller ve sonra view oluşturulur.
             
             _context.Kurslar.Add(model);
             await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
+            return RedirectToAction("Index","Kurs");
 
         }
+
+        public async Task<IActionResult> Edit(int? id){
+
+            if(id == null)
+            {
+                return NotFound();
+            }
+            
+            var kurs = await _context.Kurslar.FirstOrDefaultAsync(b => b.KursId == id); //öncekine göre daha garanti bir yöntemdir.
+            if(kurs == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(kurs);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Kurs model){
+            if(id != model.KursId){
+                return NotFound();
+            }
+
+            if(ModelState.IsValid){
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!_context.Kurslar.Any(o => o.KursId == model.KursId)){
+                        return NotFound();
+                    }
+                    else{
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    
+
 
 
     }
