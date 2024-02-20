7a41ec4419e58cc7da20fae1d2e3b8ec82621e7a
Monsterter <ibrahimsenc@gmail.com>
2024-02-20 12:22:32 +0300

    Delete problem solved

--- efcore/Controllers/OgrenciController.cs
+++ efcore/Controllers/OgrenciController.cs
@@ -45,11 +45,65 @@ namespace efcore.Controllers
 
 
             return View(ogr);
-                
-
         }
 
 
-    }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Ogrenci model){
+            if(id != model.OgrenciId){
+                return NotFound();
+            }
+
+            if(ModelState.IsValid){
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!_context.Ogrenciler.Any(o => o.OgrenciId == model.OgrenciId)){
+                        return NotFound();
+                    }
+                    else{
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
     
+         public async Task<IActionResult> Delete(int? id){
+
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            //var ogr = await _context.Ogrenciler.FindAsync(id);
+            var ogr = await _context.Ogrenciler.FirstOrDefaultAsync(b => b.OgrenciId == id); //öncekine göre daha garanti bir yöntemdir.
+            if(ogr == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(ogr);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromForm]int id){
+            var ogrenci = await _context.Ogrenciler.FindAsync(id);
+            if (ogrenci == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ogrenciler.Remove(ogrenci);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
 }
