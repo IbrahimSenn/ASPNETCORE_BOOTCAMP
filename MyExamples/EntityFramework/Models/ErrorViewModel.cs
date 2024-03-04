45324a7f9389033dcd545749ddb57be66a0ef0fd
Monsterter <ibrahimsenc@gmail.com>
2024-03-04 13:51:55 +0300

    some files deleted

--- MyExamples/EntityFramework/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EntityFramework.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
